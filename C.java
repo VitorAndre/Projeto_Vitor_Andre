b7ec0b87600e77cd1d8fc8c5da61a4ccd138ec96
Vitor <vitoraot@gmail.com>
2022-02-05 12:29:53 -0300

    Criacao da classe C

---
+++ C.java
@@ -0,0 +1,33 @@
+public class C {
+    public int C1;
+    public float C2;
+
+    C(int c1, float c2) {
+        this.C1 = c1;
+        this.C2 = c2;
+    }
+
+    public int getC1() {
+        return this.C1;
+    }
+
+    public float getC2() {
+        return this.C2;
+    }
+
+    public void setC1(int c1) {
+        this.C1 = c1;
+    }
+
+    public void setC2(int c2) {
+        this.C2 = c2;
+    }
+
+    public void MC1() {
+        System.out.print("MC1");
+    }
+
+    public void MC2() {
+        System.out.print("MC2");
+    }
+}
