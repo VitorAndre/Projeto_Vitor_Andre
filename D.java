782589061d66694294433df7094244b80f20c94e
Vitor <vitoraot@gmail.com>
2022-02-05 13:01:35 -0300

    Criada a classe D

---
+++ D.java
@@ -0,0 +1,33 @@
+public class D {
+    public int D1;
+    public float D2;
+
+    D(int d1, float d2) {
+        this.D1 = d1;
+        this.D2 = d2;
+    }
+
+    public int getD1() {
+        return this.D1;
+    }
+
+    public float getD2() {
+        return this.D2;
+    }
+
+    public void setD1(int d1) {
+        this.D1 = d1;
+    }
+
+    public void setD2(int d2) {
+        this.D2 = d2;
+    }
+
+    public void MD1() {
+        System.out.print("MD1");
+    }
+
+    public void MD2() {
+        System.out.print("MD2");
+    }
+}
