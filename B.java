a82bf201f8f3e64a884737aff0e46e29244381d0
Vitor Tenório <64609895+VitorAndre@users.noreply.github.com>
2022-02-05 12:16:45 -0300

    Create B.java

---
+++ B.java
@@ -0,0 +1,33 @@
+public class A {
+    public int A1;
+    public float A2;
+
+    A(int a1, float a2) {
+        this.A1 = a1;
+        this.A2 = a2;
+    }
+
+    public int getA1() {
+        return this.A1;
+    }
+
+    public float getA2() {
+        return this.A2;
+    }
+
+    public void setA1(int a1) {
+        this.A1 = a1;
+    }
+
+    public void setA2(int a2) {
+        this.A2 = a2;
+    }
+
+    public void MA1() {
+        System.out.print("MA1");
+    }
+
+    public void MA2() {
+        System.out.print("MA2");
+    }
+}
